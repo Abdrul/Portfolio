74099b01ead74e1b756331bb07169b91804ba5ee
Abel <josephine.abel@outlook.fr>
2022-07-04 15:20:24 +0200

    try to animate the changement page

---
+++ index.js
@@ -0,0 +1,10 @@
+const link = document.querySelector('.side a');
+const containerMain = document.querySelector('container');
+const main = document.querySelector('main');
+console.log(main);
+
+console.log(link);
+
+document.body.addEventListener('animationend', () => {
+    console.log("salt");
+})
