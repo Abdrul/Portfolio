08d0ef284177ac5a3a4b8cae1431932dc3131da1
Abel <josephine.abel@outlook.fr>
2022-07-28 16:51:26 +0200

    end change color background of portofolio page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menuCircle = document.querySelector('.menu');
 const sections = document.querySelectorAll('section');
-
+const main = document.querySelector('.container-main');
+const titleProject = document.querySelector('.title-project');
 
 
 menuCircle.addEventListener('mouseover', () => {
@@ -8,6 +9,12 @@ menuCircle.addEventListener('mouseover', () => {
         section.style.filter = "blur(4px)";
         section.style.transition = "filter 0.5s";
     };
+    
+    main.style.filter = "blur(4px)";
+    main.style.transition = "filter 0.5s";
+
+    titleProject.style.filter = "blur(4px)";
+    titleProject.style.transition = "filter 0.5s";
 });
 
 menuCircle.addEventListener('mouseleave', () => {
@@ -15,4 +22,9 @@ menuCircle.addEventListener('mouseleave', () => {
         section.style.filter = "";
         section.style.transition = "filter 0.5s";
     };
+    main.style.filter = "";
+    main.style.transition = "filter 0.5s";
+
+    titleProject.style.filter = "";
+    titleProject.style.transition = "filter 0.5s";
 });
