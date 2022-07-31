3c3d337ac6fc36ca68acc0712062a6ce09eaf1ca
Abel <josephine.abel@outlook.fr>
2022-07-31 14:43:08 +0200

    add emailjs to receive email from user

--- contact.js
+++ contact.js
@@ -1,6 +1,7 @@
 const menuCircle = document.querySelector('.menu');
 const containerContact = document.querySelector('.container-contact');
 const titleContact = document.querySelector('.title-contact');
+const formContact = document.querySelector('.form-contact');
 
 
 menuCircle.addEventListener('mouseover', () => {
@@ -20,3 +21,23 @@ menuCircle.addEventListener('mouseleave', () => {
     titleContact.style.filter = "";
     titleContact.style.transition = "filter 0.5s";
 });
+
+
+// (function() {
+//     // https://dashboard.emailjs.com/admin/account
+//     emailjs.init('403Z0LykCnUoSa-3Z');
+// })();
+
+
+// formContact.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     // generate a five digit number for the contact_number variable
+//     this.contact_number.value = Math.random() * 100000 | 0;
+//     // these IDs from the previous steps
+//     emailjs.sendForm('service_cluuob8', 'template_633sa48', this)
+//         .then(function() {
+//             console.log('SUCCESS!');
+//         }, function(error) {
+//             console.log('FAILED...', error);
+//         });
+// })
